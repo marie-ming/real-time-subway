7f0ad8e1bcc214c0e4b2a7760f68524776b574bd
ming <44185091+selene4515@users.noreply.github.com>
2022-11-21 14:23:46 +0900

    프로젝트 생성

---
+++ src/MainPage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const MainPage = () => {
+  return (
+    <div>
+      <h1>실시간 지하철!</h1>
+      <img src="" alt="지하철 map"></img>
+    </div>
+  );
+};
+
+export default MainPage;
