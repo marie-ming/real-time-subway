357c1cc04dc1687625d07e465c66e8f9351f5a5f
ming <44185091+selene4515@users.noreply.github.com>
2022-11-23 22:17:41 +0900

    UI 수정, 컴포넌트 호출 버그 수정

--- src/MainPage.jsx
+++ src/MainPage.jsx
@@ -1,41 +1,66 @@
 import FirstLine from "./components/FirstLine";
 import SecondLine from "./components/SecondLine";
+import ThirdLine from "./components/ThirdLine";
 import { useState } from "react";
-import LineButton from "./styled-components/LineButton";
+import "./main.css";
+import reload from "./reload.png";
 
 const MainPage = () => {
   const [openMap, setOpenMap] = useState(0);
 
-  // const openLine = () => {
-  //   if (openMap === 1) {
-  //     <FirstLine />;
-  //   } else if (openMap === 2) {
-  //     <SecondLine />;
-  //   } else {
-  //     <div className="numberDiv">
-  //       <img
-  //         src="https://ssl.pstatic.net/sstatic/keypage/outside/subway/img/220718/smap_sg_all.png"
-  //         alt="전체보기"
-  //         className="numberLine"
-  //       />
-  //     </div>;
-  //   }
-  // };
-  console.log(openMap);
   return (
-    <div>
-      <h1>실시간 지하철!</h1>
-      <LineButton onClick={() => setOpenMap(0)} sname={"전체보기"} />
-      <LineButton onClick={() => setOpenMap(1)} sname={"1호선"} />
-      <LineButton onClick={() => setOpenMap(2)} sname={"2호선"} />
-      <LineButton onClick={() => setOpenMap(3)} sname={"3호선"} />
-      {/*{openLine} */}
-      <div className="numberDiv">
-        <img
-          src="https://ssl.pstatic.net/sstatic/keypage/outside/subway/img/220718/smap_sg_all.png"
-          alt="전체보기"
-          className="numberLine"
-        />
+    <div className="body">
+      <div className="titleDiv">
+        <span className="title">실시간 지하철</span>
+        <button className="reload" onClick={() => window.location.reload()}>
+          <img
+            src={reload}
+            alt="새로고침"
+            style={{ width: "15px", margin: "5px" }}
+          />
+        </button>
+      </div>
+      <div className="lineDiv">
+        <button
+          className={openMap === 0 ? "lineBtn clicked" : "lineBtn"}
+          onClick={() => setOpenMap(0)}
+        >
+          전체보기
+        </button>
+        <div>
+          <button
+            className={openMap === 1 ? "lineBtn clicked" : "lineBtn"}
+            onClick={() => setOpenMap(1)}
+          >
+            1호선
+          </button>
+          <button
+            className={openMap === 2 ? "lineBtn clicked" : "lineBtn"}
+            onClick={() => setOpenMap(2)}
+          >
+            2호선
+          </button>
+          <button
+            className={openMap === 3 ? "lineBtn clicked" : "lineBtn"}
+            onClick={() => setOpenMap(3)}
+          >
+            3호선
+          </button>
+        </div>
+      </div>
+      <div className="mainDiv">
+        {openMap === 0 && (
+          <div className="numberDiv">
+            <img
+              src="https://ssl.pstatic.net/sstatic/keypage/outside/subway/img/220718/smap_sg_all.png"
+              alt="전체보기"
+              className="numberLine"
+            />
+          </div>
+        )}
+        {openMap === 1 && <FirstLine />}
+        {openMap === 2 && <SecondLine />}
+        {openMap === 3 && <ThirdLine />}
       </div>
     </div>
   );
---
+++ src/components/ThirdLine.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const ThirdLine = () => {
+  return (
+    <div className="numberDiv">
+      <img
+        src="https://ssl.pstatic.net/sstatic/keypage/outside/subway/img/220718/smap_sg3.png"
+        alt="3호선"
+        className="numberLine"
+      ></img>
+    </div>
+  );
+};
+
+export default ThirdLine;
